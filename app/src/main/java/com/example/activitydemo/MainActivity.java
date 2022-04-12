b083dc9188b4e9d8cb07fb45ed0614c0a402a1e5
a18luken <a18luken@student.his.se>
2022-04-12 13:54:15 +0200

    Added logging of MainActivity lifecycle state.

--- app/src/main/java/com/example/activitydemo/MainActivity.java
+++ app/src/main/java/com/example/activitydemo/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.activitydemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>", "I was resumed");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "I was started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "I was stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "I was destroyed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("==>", "mainactivitycreated");
     }
 }
