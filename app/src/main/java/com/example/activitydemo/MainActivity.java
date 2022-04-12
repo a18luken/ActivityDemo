7a207450585575f298a4e611cf3e20fbbdd8381c
a18luken <a18luken@student.his.se>
2022-04-12 15:03:49 +0200

    Added Intent for ProfileActivity

--- app/src/main/java/com/example/activitydemo/MainActivity.java
+++ app/src/main/java/com/example/activitydemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.activitydemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -46,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
         Log.d("==>", "Sign in Button was clicked");
+        Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+        startActivity(intent);
         }
     });
     }
